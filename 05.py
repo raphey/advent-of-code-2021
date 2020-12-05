3eca96d5a41dabe15c8791cce3e259fdeac231af
raphey <16931750+raphey@users.noreply.github.com>
2020-12-04 21:24:50 -0800

    solve 05

--- 05.py
+++ 05.py
@@ -2,39 +2,43 @@ import re
 from inputs.input_05 import sample_input, main_input
 
 
-def generate_regex_captures(string, regex):
+def get_regex_captures(string, regex):
     if 'parser' not in globals():
         global parser
         parser = re.compile(regex)
-    m = parser.match(string)
-    i = 1
-    while True:
-        try:
-            yield m.group(i)
-            i += 1
-        except IndexError:
-            break
+    m = parser.search(string)
+    return m.groups()
 
 
 def generate_parsed_input(raw_input):
-    for parsed_input_item in raw_input.split('\n'):
-        yield parsed_input_item
+    for raw_input_item in raw_input.split('\n'):
+        if not raw_input_item: continue
+        fbs, lrs = raw_input_item[:7], raw_input_item[7:]
+        yield (int(fbs.replace('F', '0').replace('B', '1'), 2),
+               int(lrs.replace('L', '0').replace('R', '1'), 2))
 
 
 def part_1(raw_input):
-    for x in generate_parsed_input(raw_input):
-        pass
+    print('Part 1')
+    max_id = 0
+    for x, y in generate_parsed_input(raw_input):
+        id = 8 * x + y
+        max_id = max(max_id, id)
+    print(max_id)
 
 
 def part_2(raw_input):
-    for x in generate_parsed_input(raw_input):
-        pass
+    print('Part 2')
+    ids = sorted([8 * x + y for x, y in generate_parsed_input(raw_input)])
+    id_set = set(ids)
+    for id in ids:
+        if id + 1 not in id_set and id + 2 in id_set:
+            print(id + 1)
 
 
 part_1(sample_input)
-# part_1(main_input)
-
+part_1(main_input)
 
 # part_2(sample_input)
-# part_2(main_input)
+part_2(main_input)
 
