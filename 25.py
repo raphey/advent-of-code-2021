f44e31b743e19a54eadd4032d0b3179dc7a90423
raphey <16931750+raphey@users.noreply.github.com>
2020-12-25 08:06:50 -0800

    25 as solved

--- 25.py
+++ 25.py
@@ -16,16 +16,46 @@ def get_parsed(raw_input):
     return parsed
 
 
-sample_input_0 = """"""
+sample_input_0 = [5764801, 17807724]
+
+
+main_input = [12578151, 5051300]
 
 sample_input_1 = """"""
 
 
+def generate_transformed_subject_numbers(sn):
+    val = 1
+    while True:
+        val *= sn
+        val %= 20201227
+        yield val
+
+
+def get_transformed_subject_number(sn, loop_size):
+    val = 1
+    for _ in range(loop_size):
+        val *= sn
+        val %= 20201227
+    return val
+
+
 def part_1(raw_input):
-    parsed = get_parsed(raw_input)
-    for x in parsed:
-        pass
-    answer = 0
+    card_pk, door_pk = raw_input
+    for i, t in enumerate(generate_transformed_subject_numbers(7)):
+        if t == card_pk:
+            card_loop = i + 1
+            break
+        if i > 1000000:
+            raise ValueError('Too much card loop iteration')
+
+    for i, t in enumerate(generate_transformed_subject_numbers(7)):
+        if t == door_pk:
+            door_loop = i + 1
+            break
+        if i > 10000000:
+            raise ValueError('Too much door loop iteration')
+    answer = get_transformed_subject_number(door_pk, card_loop)
     print(f'Part1: {answer}')
 
 
@@ -37,7 +67,7 @@ def part_2(raw_input):
     print(f'Part2: {answer}')
 
 part_1(sample_input_0)
-# part_1(main_input)
+part_1(main_input)
 
-# part_2(sample_input_1)
+# part_2(sample_input_0)
 # part_2(main_input)
