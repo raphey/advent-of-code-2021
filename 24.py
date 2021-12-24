363f6cdf19c19fe5e29f6e6960a7ff4e529362f5
raphey <16931750+raphey@users.noreply.github.com>
2021-12-23 22:33:48 -0800

    got part 1

--- 24.py
+++ 24.py
@@ -88,32 +88,86 @@ class ALU:
 def get_mutated(start_str, k):
     new_str = start_str
     for i in sample(range(13), k):
-        new_str = new_str[:i + 1] + str(randint(1, 9)) + new_str[i + 2:]  # keep leading 8
+        new_str = new_str[:i + 1] + str(randint(1, 9)) + new_str[i + 2:]  # keep leading digit
+        # new_str = new_str[:i] + str(randint(1, 9)) + new_str[i + 1:]
     return new_str
 
 def part_1(raw_input):
     instructions = get_parsed(raw_input)
     answer = ""
-    current_best_str = "89999997919839"
-    min_z = 407
-    for i in range(10000):
-        str_i = get_mutated(current_best_str, 4)
-        # if '0' in str_i:
-        #     continue
-        inputs = [int(d) for d in str_i]
-        assert len(inputs) == 14
-        alu = ALU()
-        for inst in instructions:
-            if inst[0] == "inp":
-                alu.inp(inst[1], inputs.pop(0))
-            else:
-                getattr(alu, inst[0])(inst[1], inst[2])
-        if alu.z == 0:
-            answer = str_i
-            break
-        else:
-            if alu.z < min_z:
-                print(str_i, alu.z)
+    short_list = []
+    for current_best_str, z_min in [
+        # ('81299897999854', 10),
+        # ('81299897999852', 8),
+        # ('81299897999853', 9),
+        # ('85299897999895', 11),
+        # ('81299567999854', 10),
+        # ('81299897999851', 7),
+        # ('82299897999864', 10),
+        # ('81255897999851', 7),
+        # ('81299897998754', 10),
+        # ('81299897999855', 11),
+        # ('81299897998754', 10),
+        # ('81299897999852', 8),
+        # ('81299897997654', 10),
+        # ('81299787999851', 7),
+        # ('81299897999851', 7),
+        # ('85299897999896', 0)
+        # '81299897999856',
+        #  '82299897999866',
+        #  '83299347999876',
+        #  '84266897999886',
+        #  '84299897999886',
+        #  '85211897999896',
+        #  '85222457999896',
+        #  '85222897999896',
+        #  '85233897999896',
+        #  '85244895799896',
+        #  '85244897992196',
+        #  '85244897999896',
+        #  '85255893599896',
+        #  '85266897999896',
+        #  '85277897999896',
+        #  '85288237999896',
+        #  '85288897999896',
+        #  '85299127999896',
+        #  '85299237999896',
+        #  '85299347999896',
+        #  '85299456899896',
+        #  '85299457999896',
+        #  '85299677999896',
+        #  '85299891399896',
+        #  '85299893599896',
+        #  '85299895799896',
+        #  '85299896899896',
+        #  '85299897992196',
+        #  '85299897993296',
+        #  '85299897994396',
+        #  '85299897997696',
+        #  '85299897998796',
+        #  '85299897999896'
+        ('95299897999896', 12)
+    ]:
+        for i in range(10000):
+            str_i = get_mutated(current_best_str, 4)
+            # if '0' in str_i:
+            #     continue
+            inputs = [int(d) for d in str_i]
+            assert len(inputs) == 14
+            alu = ALU()
+            for inst in instructions:
+                if inst[0] == "inp":
+                    alu.inp(inst[1], inputs.pop(0))
+                else:
+                    getattr(alu, inst[0])(inst[1], inst[2])
+            print(alu.z)
+            if alu.z == 0:
+                short_list.append(str_i)
+                # break
+            elif alu.z < z_min:
+                print(str_i, z_min)
+    print(sorted(set(short_list)))
+    print(max(short_list))
     print(f'Part1: {answer}')
 
 
